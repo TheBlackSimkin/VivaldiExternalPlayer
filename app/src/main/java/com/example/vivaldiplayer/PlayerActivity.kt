33b9b275e8a71280e0ffc7c17bfd30cd3e05578c
TheBlackSimkin <313042974+TheBlackSimkin@users.noreply.github.com>
2026-08-06 00:03:27 -0400

    Update PlayerActivity.kt

--- app/src/main/java/com/example/vivaldiplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/vivaldiplayer/PlayerActivity.kt
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
-import androidx.media3.common.SeekParameters
+import androidx.media3.exoplayer.SeekParameters
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
